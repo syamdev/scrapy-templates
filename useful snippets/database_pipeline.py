db492851b1103e5b87f8e06558c6cc79d8846f55
Attila Toth <attilatoth98@gmail.com>
2017-08-24 15:02:28 +0200

    addid database_pipeline.py

---
+++ useful snippets/database_pipeline.py
@@ -0,0 +1,54 @@
+#
+# This file was created by Attila Toth - http://scrapingauthority.com
+#
+#
+# This snippet contains a MySQL database pipeline
+# You have to add your database settings and enable the pipeline in scrapy settings.py in order to use it
+#
+# Define the db settings like this:
+#
+# DB_SETTINGS = {
+#     'db': "my_db",
+#     'user': 'root',
+#     'passwd': 'my_pass',
+#     'host': '0.0.0.0',
+# }
+#
+#
+
+
+import MySQLdb
+from scrapy.exceptions import NotConfigured
+
+
+class DatabasePipeline(object):
+
+    def __init__(self, db, user, passwd, host):
+        self.conn = MySQLdb.connect(db=db,
+                               user=user, passwd=passwd,
+                               host=host,
+                               charset='utf8', use_unicode=True)
+        self.cursor = self.conn.cursor()
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        db_settings = crawler.settings.getdict("DB_SETTINGS")
+        if not db_settings:
+            raise NotConfigured
+        db = db_settings['db']
+        user = db_settings['user']
+        passwd = db_settings['passwd']
+        host = db_settings['host']
+        return cls(db, user, passwd, host)
+
+    def process_item(self, item, spider):
+        sql = "INSERT INTO table (field1, field2, field3) VALUES (%s, %s, %s)"
+        self.cursor.execute(sql,
+                            (
+                                item.get("field1"),
+                                item.get("field2"),
+                                item.get("field3"),
+                            )
+                            )
+        self.conn.commit()
+        return item
